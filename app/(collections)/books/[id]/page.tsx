85a38392c6d9afe24d48714a99d9228f6d1c9831
Lucas Hearn <lhearnwork@gmail.com>
2025-01-06 12:48:13 -0500

    remove books pages

--- app/(collections)/books/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const BookDetailsPage = () => {
-  return <div>BookDetailsPage</div>;
-};
-
-export default BookDetailsPage;
--- app/(collections)/books/page.tsx
+++
@@ -1,3 +0,0 @@
-export default async function BooksHomePage() {
-  return <div>BooksHomePAge</div>;
-}
