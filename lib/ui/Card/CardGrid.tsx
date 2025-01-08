ef5723a6cb6deac68702cef18ac5d50addcee3f8
Lucas Hearn <lhearnwork@gmail.com>
2025-01-08 11:03:25 -0500

    add key

--- lib/ui/Card/CardGrid.tsx
+++ lib/ui/Card/CardGrid.tsx
@@ -9,7 +9,12 @@ const CardGrid = ({ items }: { items: CardData[] }) => {
   return (
     <SimpleGrid cols={5}>
       {items.map((item: CardData) => (
-        <ItemCard id={item.id} title={item.title} image={item.image} />
+        <ItemCard
+          key={item.id}
+          id={item.id}
+          title={item.title}
+          image={item.image}
+        />
       ))}
     </SimpleGrid>
   );
