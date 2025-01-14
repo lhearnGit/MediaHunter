8ffed6d03d9e9632299acc7efd8b825baba1ca92
Lucas Hearn <lhearnwork@gmail.com>
2025-01-14 13:30:54 -0500

    Re-design Games Details Page

--- app/(collections)/games/[id]/page.tsx
+++ app/(collections)/games/[id]/page.tsx
@@ -1,151 +1,257 @@
-import { Profile_Object_Payload } from "@/lib/entities/DBStorage/Collection_Payload";
-import { IGDB_Genre, Platform, ScreenShot } from "@/lib/entities/IGDB";
+import { IGDB_Genre, Platform, Theme } from "@/lib/entities/IGDB";
+import { DLC, Game_Cover, Involved_Company } from "@/lib/entities/IGDB/Games";
+
 import StyledBadges from "@/lib/ui/StyledBadges";
-import {
-  Box,
-  Container,
-  Grid,
-  GridCol,
-  Group,
-  Image,
-  Space,
-  Stack,
-  Text,
-  Title,
-} from "@mantine/core";
-import ProfileActions from "../../_components/ProfileActions";
-import { IGDB_Fetch, IGDB_Request } from "@/services/igdb-api-client";
-import { Igdb_Theme } from "@prisma/client";
+import { fetchGameDetails } from "@/utils/fetches/IGDB/fetchGameDetails";
 import IGDB_Image_Helper from "@/utils/helpers/IGDB_Image_Helper";
-import { BannerImage } from "@/lib/ui/Sections/Headers/BannerImage";
+import { Grid, GridCol, Group, Space, Stack, Text, Title } from "@mantine/core";
+import { round } from "lodash";
+import Image from "next/image";
+import Link from "next/link";
 
-async function fetchGameDetails(id: number) {
-  const request: IGDB_Request = {
-    endpoint: "games",
-    query: `fields 
-      id,name,summary,
-      websites.url,
-      rating,rating_count,
-      first_release_date,
-      involved_companies.company.*,
-      cover.url,
-      genres.slug,genres.name,
-      themes.slug,themes.name,
-      videos.video_id,videos.name,
-      screenshots.url, screenshots.id,
-      age_ratings,
-      platforms.id, platforms.name, platforms.slug, platforms.platform_family.id;
-      
-      
-      where id=${id};`,
-  };
-  const details = await IGDB_Fetch(request);
-
-  return details.pop();
-}
 const GamesDetailsPage = async ({ params }: { params: { id: number } }) => {
-  const userId = "66d73678a5ae02f237ead4d9";
-  const endpoint = "movies";
   const {
-    genres,
-    themes,
-    summary,
     cover,
     name,
-    screenshots,
-    videos,
-    involved_companies,
+    genres,
+    themes,
     platforms,
-    websites,
+    rating,
+    rating_count,
+    aggregated_rating,
+    aggregated_rating_count,
+    summary,
+    storyline,
+    videos,
+    screenshots,
+    similar_games,
+    dlcs,
+    companies,
   } = await fetchGameDetails(params.id);
-  const image = IGDB_Image_Helper(cover.url, "1080p");
 
-  const payload: Profile_Object_Payload = {
-    itemId: params.id,
-    imageUrl: image,
-    name: name,
-  };
-
-  console.log(websites);
   return (
-    <Container size={"xl"}>
-      <Title>{name}</Title>
+    <Grid>
+      <GridCol span={12}>
+        <Group justify="space-between">
+          <Title>{name}</Title>
+        </Group>
+        <Space h="xl" />
+      </GridCol>
+      <GridCol span={8}>
+        <Grid columns={8}>
+          <GridCol span={7}>
+            <Title mb={"md"}>Summary</Title>
+            <Text mb={10}>{summary}</Text>
+            {storyline != summary && storyline ? (
+              <Text>{storyline}</Text>
+            ) : (
+              <></>
+            )}
+            <br />
+          </GridCol>
+          <GridCol span={4}>
+            <GenresAndThemes genres={genres} themes={themes} />
+            <AvailablePlatforms platforms={platforms} />
+          </GridCol>
+        </Grid>
+        {dlcs && <DLCContent content={dlcs} />}
+      </GridCol>
+      <GridCol offset={1} span={3}>
+        <Ratings
+          rating={rating}
+          rating_count={rating_count}
+          aggregated_rating={aggregated_rating}
+          aggregated_rating_count={aggregated_rating_count}
+        />
+        <ReleaseDates />
+        <Companies companies={companies} />
+      </GridCol>
 
-      <Space h="xl" />
-      <Grid>
-        <GridCol span={8}>
-          <Stack>
-            <Text size="lg">Genres</Text>
-            <Group>
-              {genres &&
-                genres.map((genre: IGDB_Genre) => (
-                  <StyledBadges
-                    key={genre.id}
-                    label={genre.name}
-                    color="blue"
-                  />
-                ))}
-              {themes &&
-                themes.map((genre: Igdb_Theme) => (
-                  <StyledBadges
-                    key={genre.id}
-                    label={genre.name}
-                    color="blue"
-                  />
-                ))}
-              {platforms &&
-                platforms.map((genre: Platform) => (
-                  <StyledBadges
-                    key={genre.id}
-                    label={genre.name}
-                    color="blue"
-                  />
-                ))}
-            </Group>
-            <Text>{summary}</Text>
-            <Stack>
-              {websites &&
-                websites.map((website: { id: number; url: string }) => (
-                  <a>{website.url}</a>
-                ))}
-            </Stack>
-            <Stack>
-              {involved_companies &&
-                involved_companies.map((website: any) => (
-                  <>
-                    <p>{website.company.name}</p>
-                    <p>{website.company.url}</p>
-                  </>
-                ))}
-            </Stack>
-
-            <Grid>
-              <GridCol span={6}>
-                {screenshots &&
-                  screenshots.map((screenshot: ScreenShot) => (
-                    <Image
-                      radius="sm"
-                      src={IGDB_Image_Helper(screenshot.url, "720p")}
-                      alt="no img"
-                    />
-                  ))}
-              </GridCol>
-            </Grid>
-          </Stack>
-        </GridCol>
-        <GridCol span={4}>
-          <Stack bg={"dark"} px={5} py={10}>
-            <Image radius="sm" src={image} alt="no img" />
-            <ProfileActions
-              endpoint={endpoint}
-              userId={userId}
-              payload={payload}
-            />
-          </Stack>
-        </GridCol>
-      </Grid>
-    </Container>
+      <GridCol span={12}>
+        <SimilarGames games={similar_games} />
+      </GridCol>
+    </Grid>
   );
 };
 
 export default GamesDetailsPage;
+
+//          <GridCol span={4}>{videos && <VideoPlayer videos={videos} />}</GridCol>
+
+const DLCContent = ({ content }: { content: DLC[] }) => {
+  return (
+    <Stack>
+      <Text size="lg">DLC's</Text>
+      <Grid columns={4}>
+        {content.map(({ id, name, total_rating, cover }: DLC) => (
+          <GridCol key={id} span={1}>
+            <Stack>
+              <Image
+                style={{
+                  borderRadius: 5,
+                  boxShadow: "2px 2px 2px black",
+                }}
+                height={160}
+                width={120}
+                src={
+                  cover?.url
+                    ? IGDB_Image_Helper(cover?.url, "720p")
+                    : "/images/notfound.jpg"
+                }
+                alt="No Image found"
+              />
+              {total_rating ? (
+                <StyledBadges
+                  label={`Rating ${round(total_rating, 1)}`}
+                  color=""
+                />
+              ) : (
+                <StyledBadges label={`No Rating Available`} color="" />
+              )}
+            </Stack>
+          </GridCol>
+        ))}
+      </Grid>
+    </Stack>
+  );
+};
+const GenresAndThemes = ({
+  genres,
+  themes,
+}: {
+  genres: IGDB_Genre[];
+  themes: Theme[];
+}) => {
+  return (
+    <Stack>
+      <Text size="lg">Genres & Themes</Text>
+      <Group>
+        {genres &&
+          genres.map((genre: IGDB_Genre) => (
+            <StyledBadges key={genre.id} label={genre.name} color="blue" />
+          ))}
+        {themes &&
+          themes.map((theme: Theme) => (
+            <StyledBadges key={theme.id} label={theme.name} color="blue" />
+          ))}
+      </Group>
+    </Stack>
+  );
+};
+
+const AvailablePlatforms = ({ platforms }: { platforms: Platform[] }) => {
+  return (
+    <Stack>
+      <Text size="lg">Playable On</Text>
+      <Group>
+        {platforms &&
+          platforms.map((platform: Platform) => (
+            <StyledBadges
+              key={platform.id}
+              label={platform.name}
+              color="blue"
+            />
+          ))}
+      </Group>
+    </Stack>
+  );
+};
+
+const Ratings = ({
+  rating,
+  rating_count,
+  aggregated_rating,
+  aggregated_rating_count,
+}: {
+  rating: number;
+  rating_count: number;
+  aggregated_rating: number;
+  aggregated_rating_count: number;
+}) => {
+  return (
+    <Stack>
+      <Text size="lg">Ratings</Text>
+      <Group justify="space-between">
+        <div>
+          <Text>Player Score</Text>
+          <StyledBadges label={`${rating}% of ${rating_count}`} color="" />
+        </div>
+        <div>
+          <Text>Critic Score</Text>
+          <StyledBadges
+            label={`Critic Score ${aggregated_rating}% of ${aggregated_rating_count}`}
+            color=""
+          />
+        </div>
+      </Group>
+    </Stack>
+  );
+};
+
+const ReleaseDates = ({}: {}) => {
+  const releases = [1, 2, 3, 4];
+  return (
+    <Stack>
+      <Text size="lg">Releases</Text>
+      <Group justify="space-between">
+        {releases.map((release: number) => (
+          <p>{release}</p>
+        ))}
+      </Group>
+    </Stack>
+  );
+};
+const SimilarGames = ({ games }: { games: Game_Cover[] }) => {
+  return (
+    <>
+      <Title mb={"lg"}>Similar Games</Title>
+      <Group>
+        {games.map(({ id, cover }: Game_Cover) => (
+          <Link key={id} href={`/games/${id}`}>
+            <Image
+              style={{
+                borderRadius: 5,
+                boxShadow: "2px 2px 2px black",
+              }}
+              height={160}
+              width={120}
+              src={IGDB_Image_Helper(cover.url, "720p")}
+              alt="no image"
+            />
+          </Link>
+        ))}
+      </Group>
+    </>
+  );
+};
+const Companies = ({ companies }: { companies: Involved_Company[] }) => {
+  return (
+    <Stack>
+      <Text size="lg">Publishing & Developers</Text>
+      <Grid columns={4}>
+        {companies.map(
+          ({ id, company, publisher, developer }: Involved_Company) => (
+            <GridCol key={id} span={2}>
+              <Stack>
+                <Text>
+                  {publisher ? "Developer " : " Publisher "} {company.name}
+                </Text>
+                <Image
+                  height={64}
+                  width={64}
+                  src={
+                    company.logo?.url
+                      ? IGDB_Image_Helper(company.logo.url, "1080p")
+                      : "/images/notfound.jpg"
+                  }
+                  className="bg-inherit"
+                  alt="No Image found"
+                />
+              </Stack>
+            </GridCol>
+          )
+        )}
+      </Grid>
+    </Stack>
+  );
+};
