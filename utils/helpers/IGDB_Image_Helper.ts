e4895000b098c9627bcfd61792e5e2625d589e08
Lucas Hearn <lhearnwork@gmail.com>
2025-01-16 13:19:31 -0500

    update imagehelper to handle urls that may not be defaulted to t_thumb

--- utils/helpers/IGDB_Image_Helper.ts
+++ utils/helpers/IGDB_Image_Helper.ts
@@ -2,6 +2,10 @@ const IGDB_Image_Helper = (
   url: string,
   size: "720p" | "1080p" | "micro" | "thumb" | "logo_med" | "cover_big"
 ) => {
-  return "https:" + url.replace("t_thumb", `t_${size}`);
+  //strings are patterned t_
+  const pattern = /t_(720p|1080p|micro|thumb|logo_med|cover_big)/;
+  //if the string already starts w/ https do not concat
+  if (url.slice(0, 6) == "https:") return url.replace(pattern, `t_${size}`);
+  return "https:" + url.replace(pattern, `t_${size}`);
 };
 export default IGDB_Image_Helper;
