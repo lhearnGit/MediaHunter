63f8ebff5dd98d38dcf79071e60f9b3297174669
Lucas Hearn <lhearnwork@gmail.com>
2025-01-20 14:43:29 -0500

    Update styling, create and apply CSS module

--- app/(collections)/_components/MediaSummary.tsx
+++ app/(collections)/_components/MediaSummary.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { IGDB_Genre, Theme } from "@/lib/entities/IGDB";
-import useUpdateCollection from "@/lib/hooks/profile/useUpdateCollection";
 import StyledBadges from "@/lib/ui/StyledBadges";
-import { Grid, GridCol, Group, Text } from "@mantine/core";
-import Image from "next/image";
-import Link from "next/link";
+import { GridCol, Group, Stack, Text } from "@mantine/core";
+
+import ImageLink from "@/lib/ui/ImageLink/ImageLink";
+import classes from "./MediaSummary.module.css";
 interface SummaryProps {
   id: number;
   title: string;
@@ -13,7 +13,6 @@ interface SummaryProps {
   themes: Theme[];
   rating: number;
   rating_count: number;
-  pagePath: string;
   summary: string;
 }
 
@@ -24,41 +23,28 @@ const MediaSummary = ({
   genres,
   themes,
   summary,
-  pagePath,
   rating,
   rating_count,
 }: SummaryProps) => {
-  const updateCollection = useUpdateCollection(
-    "66d73678a5ae02f237ead4d9",
-    "games"
-  );
-
   return (
-    <Grid columns={9} gutter={"md"} mx={"sm"} m={"md"}>
-      <GridCol span={5}>
-        <Group justify="space-between" pr={"lg"}>
-          <Text key={id} size="xl">
-            <Link href={`${pagePath}/${id}`} className="no-underline">
-              {title}
-            </Link>
-          </Text>
+    <Group className={classes.wrapper}>
+      <ImageLink
+        height={300}
+        pathname="games"
+        poster={{ id: id, imageUrl: image, name: title }}
+      />
+      <Stack className={classes.details}>
+        <Group>
+          <StyledBadges
+            label={`ratings ${rating_count}`}
+            color={rating > 75 ? "green" : "red"}
+          />
+          <StyledBadges
+            label={`Player Score ${rating}`}
+            color={rating > 75 ? "green" : "red"}
+          />
         </Group>
-      </GridCol>
-      <GridCol span={4} />
-      <GridCol span={1} mr={"xl"}>
-        <Image
-          style={{
-            borderRadius: 5,
-            boxShadow: "2px 2px 2px black",
-          }}
-          width={140}
-          height={200}
-          src={image}
-          alt="no image found"
-        />
-      </GridCol>
-      <GridCol span={7}>
-        <Group pb={10}>
+        <Group className={classes.badges}>
           {themes &&
             themes.map((theme: Theme) => (
               <StyledBadges key={theme.id} label={theme.name} color="blue" />
@@ -71,35 +57,8 @@ const MediaSummary = ({
         <Text mt={"lg"} lineClamp={4}>
           {summary}
         </Text>
-      </GridCol>
-      <GridCol span={6}>
-        <Group justify="flex-end">
-          <Group>
-            <button
-              onClick={() => {
-                updateCollection.mutate({
-                  addTo: false,
-                  data: { id: id, url: image, name: title },
-                });
-              }}
-            >
-              Add To Collection
-            </button>
-            <StyledBadges
-              label={`ratings ${rating_count}`}
-              color={rating > 75 ? "green" : "red"}
-            />
-            <StyledBadges
-              label={`Player Score ${rating}`}
-              color={rating > 75 ? "green" : "red"}
-            />
-          </Group>
-          <Link href={`${pagePath}/${id}`} className="underline-offset-1">
-            Read More
-          </Link>
-        </Group>
-      </GridCol>
-    </Grid>
+      </Stack>
+    </Group>
   );
 };
 
