e034c14c6a8d93d5be7c52de88a8185547294182
Lucas Hearn <lhearnwork@gmail.com>
2025-01-20 14:43:08 -0500

    update collections layout

--- app/(collections)/layout.tsx
+++ app/(collections)/layout.tsx
@@ -1,11 +1,7 @@
-import "@mantine/core/styles.css";
-
-import { Container } from "@mantine/core";
-
 export default function CollectionsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <Container size={"xl"}>{children}</Container>;
+  return <div>{children}</div>;
 }
