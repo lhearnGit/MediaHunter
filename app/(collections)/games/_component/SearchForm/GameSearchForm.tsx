2e25630ab07deb58593235b6e0f5399e1ae5be9e
Lucas Hearn <lhearnwork@gmail.com>
2025-01-22 16:10:43 -0500

    update styling, add Collapse to form

--- app/(collections)/games/_component/SearchForm/GameSearchForm.tsx
+++ app/(collections)/games/_component/SearchForm/GameSearchForm.tsx
@@ -1,10 +1,18 @@
 "use client";
-import { Button, Checkbox, SimpleGrid, Stack } from "@mantine/core";
+import {
+  Button,
+  Checkbox,
+  Collapse,
+  Group,
+  SimpleGrid,
+  Stack,
+} from "@mantine/core";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { SubmitHandler, useForm } from "react-hook-form";
 import classes from "./GameSearchForm.module.css";
 import { IGDB_Genre, Theme } from "@/lib/entities/IGDB";
 import { ArrayToCSV } from "@/utils/helpers/stringFns";
+import { useDisclosure } from "@mantine/hooks";
 
 type Inputs = {
   genres: string[];
@@ -19,6 +27,7 @@ const GameSearchForm = ({ genres, themes }: FormData) => {
   const searchParams = useSearchParams();
   const router = useRouter();
   const pathname = usePathname();
+  const [opened, { toggle }] = useDisclosure(false);
 
   const {
     register,
@@ -39,72 +48,78 @@ const GameSearchForm = ({ genres, themes }: FormData) => {
     } else {
       params.append("themes", themes);
     }
+    toggle();
     router.replace(`${pathname}/?${params.toString()}`);
   };
 
   return (
-    <div className={classes.wrapper}>
-      <form
-        id="gamesearchform"
-        className={classes.formWrapper}
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <SimpleGrid className={classes.grid} cols={{ base: 1, xl: 2 }}>
-          <Stack className={classes.stack}>
-            <h1>Genres</h1>
-            <SimpleGrid
-              className={classes.subGrid}
-              cols={{ base: 3, lg: 4, xl: 5 }}
-            >
-              {genres.map(({ name, id }: IGDB_Genre) => (
-                <Checkbox
-                  key={id}
-                  {...register("genres")}
-                  defaultChecked={searchParams.has("genres", id.toString())}
-                  value={id}
-                  label={name}
-                />
-              ))}
+    <Stack>
+      <Group>
+        <h1 className={classes.title}>Search for Games</h1>
+        <button className="w-full" onClick={toggle}>
+          filter
+        </button>
+      </Group>
+      <div className={classes.wrapper}>
+        <Collapse in={opened}>
+          <form
+            id="gamesearchform"
+            className={classes.formWrapper}
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <SimpleGrid className={classes.grid} cols={{ base: 1, xl: 2 }}>
+              <Stack className={classes.stack}>
+                <h1>Genres</h1>
+                <SimpleGrid
+                  className={classes.subGrid}
+                  cols={{ base: 3, lg: 4, xl: 5 }}
+                >
+                  {genres.map(({ name, id }: IGDB_Genre) => (
+                    <Checkbox
+                      key={id}
+                      {...register("genres")}
+                      defaultChecked={searchParams.has("genres", id.toString())}
+                      value={id}
+                      label={name}
+                    />
+                  ))}
+                </SimpleGrid>
+              </Stack>
+              <Stack className={classes.stack}>
+                <h1>Themes</h1>
+                <SimpleGrid
+                  className={classes.subGrid}
+                  cols={{ base: 3, lg: 4, xl: 5 }}
+                >
+                  {themes.map(({ name, id }: Theme) => (
+                    <Checkbox
+                      key={id}
+                      {...register("themes")}
+                      defaultChecked={searchParams.has("themes", id.toString())}
+                      value={id}
+                      label={name}
+                    />
+                  ))}
+                </SimpleGrid>
+              </Stack>
             </SimpleGrid>
-          </Stack>
-          <Stack className={classes.stack}>
-            <h1>Themes</h1>
-            <SimpleGrid
-              className={classes.subGrid}
-              cols={{ base: 3, lg: 4, xl: 5 }}
+            <Button type="submit" className={classes.submit}>
+              Search
+            </Button>
+            <Button
+              type="reset"
+              onClick={() => {
+                router.replace(`${pathname}`);
+              }}
+              className={classes.submit}
             >
-              {themes.map(({ name, id }: Theme) => (
-                <Checkbox
-                  key={id}
-                  {...register("themes")}
-                  defaultChecked={searchParams.has("themes", id.toString())}
-                  value={id}
-                  label={name}
-                />
-              ))}
-            </SimpleGrid>
-          </Stack>
-        </SimpleGrid>
-        <Button type="submit" className={classes.submit}>
-          Search
-        </Button>
-        <Button
-          type="reset"
-          onClick={() => {
-            router.replace(`${pathname}`);
-          }}
-          className={classes.submit}
-        >
-          Clear
-        </Button>
-      </form>
-    </div>
+              Clear
+            </Button>
+          </form>
+        </Collapse>
+      </div>
+    </Stack>
   );
 };
 
 export default GameSearchForm;
-
-/*
-
-
-*/
