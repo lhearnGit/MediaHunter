e29fe950ce6c4c699a82c301ebec93f3009175c8
Lucas Hearn <lhearnwork@gmail.com>
2025-01-22 15:55:34 -0500

    update CSS modules, remove links and Param Container
    Param Container replaced by Games/search page

--- app/(collections)/games/page.tsx
+++ app/(collections)/games/page.tsx
@@ -1,12 +1,10 @@
 import { IGDB_Fetch } from "@/services/igdb-api-client-v2";
 import { isValidUrlParam } from "@/utils/zodSchemas/UrlSchema";
-import Link from "next/link";
-import SearchParamContainer from "../_components/SearchParamContainer";
 import BrowseGames from "./_component/BrowseGames";
-
+import classes from "./GamesHomePage.module.css";
 import { Game } from "@/lib/entities/IGDB";
 import { setQuery } from "@/utils/fetches/IGDB/Queries/GameHomeQueries";
-import classes from "./GamesHomePage.module.css";
+
 const GamesHome = async ({
   searchParams,
 }: {
@@ -29,36 +27,20 @@ const GamesHome = async ({
     page = parseInt(params.get("page")!); //null and NaN cases are handled above
   }
 
-  const theme = params.get("themes");
-  const genre = params.get("genres");
   let option: string | null = params.has("option")
     ? params.get("option")
-    : "recent";
+    : null;
   console.log(params);
   const games = await IGDB_Fetch<Game>({
     endpoint: "games",
-    query: setQuery(page, option, genre, theme),
+    query: setQuery(page, option, null, null),
   });
 
   return (
-    <div>
-      <div className={classes.navbar}>
-        <Link className={classes.navlink} href={`games?option=recent`} replace>
-          Recent Releases
-        </Link>
-        <Link
-          className={classes.navlink}
-          href={`games?option=upcoming`}
-          replace
-        >
-          Upcoming Releases
-        </Link>
-        <Link className={classes.navlink} href={`games`} replace>
-          Clear
-        </Link>
-      </div>
-      <h1>{option} Games</h1>
-      <SearchParamContainer />
+    <div className={classes.wrapper}>
+      <h1 className={classes.header}>
+        {option ? option : "Recently Released"} Games
+      </h1>
       <BrowseGames games={games} />
     </div>
   );
