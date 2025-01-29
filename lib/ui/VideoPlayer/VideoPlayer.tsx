5d44f3627977f4d458efbf688263e1bd133f1e4c
Lucas Hearn <lhearnwork@gmail.com>
2025-01-29 13:24:23 -0500

    update video player to be reactive in size

--- lib/ui/VideoPlayer/VideoPlayer.tsx
+++ lib/ui/VideoPlayer/VideoPlayer.tsx
@@ -1,33 +1,38 @@
 "use client";
 
 import { Video } from "@/lib/entities/IGDB";
-import { Button, Grid } from "@mantine/core";
+import { Button, SimpleGrid, Space, Stack } from "@mantine/core";
 import { useState } from "react";
 import ReactPlayer from "react-player";
-
+import classes from "./videoplayer.module.css";
 const VideoPlayer = ({ videos }: { videos: Video[] }) => {
   const [loadedVideo, setLoadedVideo] = useState<Video>(videos[0]);
   if (!videos) return <p>No Videos</p>;
   return (
-    <Grid>
-      <Grid.Col span={12}>
+    <Stack>
+      <div className={classes.playerWrapper}>
         <ReactPlayer
-          className="shadow-xl shadow-black my-auto"
+          height={"90%"}
+          width={"90%"}
+          className={classes.reactPlayer}
           url={`https://www.youtube.com/watch?v=${loadedVideo.video_id}`}
         />
-      </Grid.Col>
-      {videos?.map((video: Video) => (
-        <Button
-          color={video.id == loadedVideo.id ? "gray" : ""}
-          key={video.id}
-          onClick={() => {
-            setLoadedVideo(video);
-          }}
-        >
-          {video.name}
-        </Button>
-      ))}
-    </Grid>
+      </div>
+
+      <SimpleGrid cols={4} w={"90%"}>
+        {videos?.map((video: Video) => (
+          <Button
+            color={video.id == loadedVideo.id ? "gray" : ""}
+            key={video.id}
+            onClick={() => {
+              setLoadedVideo(video);
+            }}
+          >
+            {video.name}
+          </Button>
+        ))}
+      </SimpleGrid>
+    </Stack>
   );
 };
 
