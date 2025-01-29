fef995385d668c09ac78bf69a80676d6de7b30dd
Lucas Hearn <lhearnwork@gmail.com>
2025-01-29 15:27:32 -0500

    fix css error

--- lib/ui/ImageLink/ImageLink.tsx
+++ lib/ui/ImageLink/ImageLink.tsx
@@ -12,22 +12,21 @@ const ImageLink = ({
   poster: Poster;
   pathname: "games/details" | "movies" | "tv";
 }) => {
+  const width = height * (2 / 3);
   return (
-    <div className={classes.wrapper}>
-      <Link
-        className={classes.link}
-        key={poster.id}
-        href={`/${pathname}/${poster.id}`}
-      >
-        <Image
-          className={classes.image}
-          height={height}
-          width={height * (2 / 3)}
-          src={poster.imageUrl ? poster.imageUrl : "/images/notfound.jpg"}
-          alt="/images/notfound.jpg"
-        />
-      </Link>
-    </div>
+    <Link
+      className={classes.link}
+      key={poster.id}
+      href={`/${pathname}/${poster.id}`}
+    >
+      <Image
+        className={classes.image}
+        height={height}
+        width={width}
+        src={poster.imageUrl ? poster.imageUrl : "/images/notfound.jpg"}
+        alt="/images/notfound.jpg"
+      />
+    </Link>
   );
 };
 
