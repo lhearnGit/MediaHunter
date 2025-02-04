22c0deaa7531eb0eddf2935cc8f8eeb73d73500a
Lucas Hearn <lhearnwork@gmail.com>
2025-02-04 11:39:47 -0500

    update fields to match source, add english_name

--- lib/entities/TMDB/Country.ts
+++ lib/entities/TMDB/Country.ts
@@ -2,7 +2,8 @@ import { z } from "zod";
 
 export const isValidCountry = z.object({
   iso_3166_1: z.string(),
-  name: z.string(),
+  english_name: z.string(),
+  native_name: z.string(),
 });
 
 export type Country = z.infer<typeof isValidCountry>;
