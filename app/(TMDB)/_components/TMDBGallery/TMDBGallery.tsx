c6acd619df17786ef9d7103cd74b5af2970af0b9
Lucas Hearn <lhearnwork@gmail.com>
2025-02-05 11:54:47 -0500

    create and apply css module, replace ImageLink as extraction is uncessary

--- app/(TMDB)/_components/TMDBGallery/TMDBGallery.tsx
+++ app/(TMDB)/_components/TMDBGallery/TMDBGallery.tsx
@@ -1,8 +1,9 @@
 import Poster from "@/lib/entities/Poster";
-import ImageLink from "@/lib/ui/ImageLink/ImageLink";
-import { TMDB_Image_Helper } from "@/utils/helpers/TMDB_Image_Helper";
-
 import { Container, SimpleGrid } from "@mantine/core";
+import Image from "next/image";
+import Link from "next/link";
+import classes from "./TMDBGallery.module.css";
+import { TMDB_Image_Helper } from "@/utils/helpers/TMDB_Image_Helper";
 const TMDBGallery = ({
   posters,
   pathname,
@@ -15,17 +16,23 @@ const TMDBGallery = ({
     <Container size={"lg"} py={20}>
       <SimpleGrid cols={{ base: 2, sm: 3, md: 5 }}>
         {posters.map((poster: Poster) => (
-          <ImageLink
-            height={240}
-            pathname={pathname}
-            poster={{
-              ...poster,
-              imageUrl: poster.imageUrl
-                ? TMDB_Image_Helper(poster.imageUrl, "original")
-                : "/images/notfound.jpg",
-            }}
+          <Link
+            className={classes.link}
             key={poster.id}
-          />
+            href={`/${pathname}/${poster.id}`}
+          >
+            <Image
+              className={classes.image}
+              height={264}
+              width={196}
+              src={
+                poster.imageUrl
+                  ? TMDB_Image_Helper(poster.imageUrl, "w500")
+                  : "/images/notfound.jpg"
+              }
+              alt={`${poster.name}`}
+            />
+          </Link>
         ))}
       </SimpleGrid>
     </Container>
