aa7de6b2d242357002313962b6414aa26e255f6b
Lucas Hearn <lhearnwork@gmail.com>
2025-02-07 16:23:56 -0500

    add null as a valid option for imageUrl

--- lib/entities/Poster.ts
+++ lib/entities/Poster.ts
@@ -3,6 +3,6 @@ import { z } from "zod";
 export const isValidPoster = z.object({
   id: z.number().int("is not an integer").gt(0, "cannot be negative"),
   name: z.string(),
-  imageUrl: z.string().or(z.undefined()),
+  imageUrl: z.string().or(z.null()).optional(),
 });
 export type Poster = z.infer<typeof isValidPoster>;
